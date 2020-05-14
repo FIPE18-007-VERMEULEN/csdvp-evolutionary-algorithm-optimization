62f4c9fb13fbce3a17f7cb68b1abb630e4d91c78
jerem <jerem@port-45199.imtld.local>
2020-05-14 09:50:44 +0200

    mut + cross

--- src/model/ea/initializer.h
+++ src/model/ea/initializer.h
@@ -20,39 +20,25 @@ class eoInitCSDVP: public eoInit<EOT>
 
     typedef typename EOT::AtomType AtomType;
 
-        eoInitCSDVP(unsigned chromSize, unsigned maxVal, int _seed)
-            : _chromSize(chromSize), _maxVal(maxVal)
+        eoInitCSDVP(unsigned _chromSize, unsigned _maxVal, int _seed)
+	  : chromSize(_chromSize), maxVal(_maxVal), seed(_seed)
         {}
 
         virtual void operator()(EOT& chrom)
         {
-            chrom.resize(_chromSize);
-
-            std::vector<int> rdVec;
-            for(int i = 0; i < this->_maxVal; i++)
-                rdVec.push_back(i);
-            std::random_shuffle(rdVec.begin(), rdVec.end());
-
-            std::vector<int> r2; //trying tmp struct
-            for(int i = 0; i < rdVec.size(); i++)
-                r2.push_back(rdVec.at(i));
-
-            for(unsigned idx=0;idx <chrom.size();idx++)
-            {
-                unsigned int tmp = r2.at(idx);
-                // chrom[idx]=tmp; // DOES NOT WORK
-                chrom[idx] = idx; //DOES WORK;
-            }
-
-            //std::random_shuffle(chrom.begin(), chrom.end(),gen);
-            chrom.invalidate();
+	  chrom.resize(0);
+	  for(int i = 0; i < maxVal; i++)
+	    chrom.push_back(i);
+	  std::random_shuffle(chrom.begin(), chrom.end());
+	  chrom.resize(chromSize);
+          chrom.invalidate();
         }
 
     private :
-        unsigned _chromSize;
-        unsigned _maxVal;
+        unsigned chromSize;
+        unsigned maxVal;
         UF_random_generator<unsigned int> gen;
-        int _seed;
+        int seed;
 };
 
 typedef eoInitCSDVP<Cursus> CursusInit;
